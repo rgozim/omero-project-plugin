45bd7dfb5262d115d9e8cb3a38ec1312b081ec22
Riad Gozim <rgozim1@gmail.com>
2019-06-28 12:22:27 +0100

    versionless

--- src/main/kotlin/org/openmicroscopy/ProjectPlugin.kt
+++ src/main/kotlin/org/openmicroscopy/ProjectPlugin.kt
@@ -29,7 +29,7 @@ class ProjectPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         apply<AdditionalRepositoriesPlugin>()
         apply<AdditionalArtifactsPlugin>()
-        // apply<ReleasePlugin>()
+        apply<ReleasePlugin>()
         apply<PublishingPlugin>()
     }
 }
