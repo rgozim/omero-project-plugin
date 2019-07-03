8a42d0cbe50d19181a069c1e6749080d3283ae62
Riad Gozim <rgozim1@gmail.com>
2019-07-03 12:09:18 +0100

    Added project release and plugin release plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,26 @@ pluginManagement {
             }
         }
     }
+
+    resolutionStrategy {
+        eachPlugin {
+            // Work around https://github.com/gradle/gradle/issues/1697.
+            if (requested.version == null && requested.id.namespace == "org.openmicroscopy") {
+                val pluginEnvName =
+                        requested.id.name.split('-')
+                                .joinToString(separator = "_").toUpperCase() + "_VERSION"
+                val propertyName =
+                        requested.id.name.split('-')
+                                .joinToString(separator = "") { it.capitalize() }
+                                .decapitalize() + "Version"
+
+                val version: Any = gradle.rootProject.findProperty(pluginEnvName)
+                        ?: gradle.rootProject.findProperty(propertyName)!!
+
+                useVersion(version.toString())
+            }
+        }
+    }
 }
 
 rootProject.name = "omero-project-plugin"
