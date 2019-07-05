58f5fba874a247a4cb045b137928b6be648a2b9c
Riad Gozim <rgozim1@gmail.com>
2019-07-05 16:27:44 +0100

    Added version override functionality to settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,29 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         maven { url = uri("https://artifacts.openmicroscopy.org/artifactory/maven") }
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            // Work around https://github.com/gradle/gradle/issues/1697.
-            if (requested.id.namespace == "org.openmicroscopy") {
-                val pluginEnvName =
-                        requested.id.name.split('-')
-                                .joinToString(separator = "_").toUpperCase() + "_VERSION"
-                val propertyName =
-                        requested.id.name.split('-')
-                                .joinToString(separator = "") { it.capitalize() }
-                                .decapitalize() + "Version"
-
-                val version: Any? = gradle.rootProject.findProperty(pluginEnvName)
-                        ?: gradle.rootProject.findProperty(propertyName)
-
-                if (version != null) {
-                    useVersion(version.toString())
-                }
-            }
-        }
-    }
-
+    
     resolutionStrategy {
         eachPlugin {
             // Work around https://github.com/gradle/gradle/issues/1697.
